84cf9e3b6ac73bd7fecbe1cf755ff8d4683a4220
Baxter Eaves <baxyjr@gmail.com>
2014-11-10 10:36:07 -0500

    command line arguments

--- test/inference/geweke/geweke_test.cpp
+++ test/inference/geweke/geweke_test.cpp
@@ -19,17 +19,63 @@
 #include <vector>
 #include <string>
 
-int main(){
+#include "boost/program_options.hpp"
 
-    // std::vector<std::string> datatypes = {"categorical", "continuous", "categorical",
-    //                                       "continuous","categorical"};
-    std::vector<std::string> datatypes = {"categorical"};
+int main(int argc, char** argv)
+{
+    // default values
+    size_t iters, chains, rows, seed, lag;
+    bool fix_hypers, fix_row_alpha, fix_col_alpha, fix_row_z, fix_col_z;
+    
+    namespace po = boost::program_options; 
+    po::options_description desc("Options"); 
+    desc.add_options() 
+        ("help", "Print help messages")
+        ("fix_hypers,y", po::bool_switch(&fix_hypers)->default_value(false), "fix hyperparamters transition")
+        ("fix_row_alpha,a", po::bool_switch(&fix_row_alpha)->default_value(false), "fix row in views CRP alpha transition")
+        ("fix_col_alpha,v", po::bool_switch(&fix_col_alpha)->default_value(false), "fix cols in state CRP alpha transition")
+        ("fix_row_z,w", po::bool_switch(&fix_row_z)->default_value(false), "fix row assignment")
+        ("fix_col_z,z", po::bool_switch(&fix_col_z)->default_value(false), "fix column assignment")
+        ("iters,i", po::value<size_t>(&iters)->default_value(10000), "number of iterations per chain")
+        ("chains,h", po::value<size_t>(&chains)->default_value(5), "number of chains")
+        ("rows,r", po::value<size_t>(&rows)->default_value(10), "number of rows")
+        ("seed,s", po::value<size_t>(&seed)->default_value(1281990), "random number generator seed")
+        ("lag,l", po::value<size_t>(&lag)->default_value(5), "number of iterations between sample collection");
 
-	baxcat::GewekeTester gwk(20, 1, datatypes, 84715, true, true, true);
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc),  vm); // can throw 
 
-    size_t lag = 5;
+    
+    // if (vm.count("help")){ 
+    //     std::cout << "Basic Command Line Parameter App" << std::endl << desc << std::endl;
+    //     return 0; 
+    // } 
 
-	gwk.run(100000, 5, lag);
+    po::notify(vm);
+
+    std::cout << "=================================================" << std::endl;
+    std::cout << "fix col alpha: " << fix_col_alpha << std::endl
+              << "fix row alpha: " << fix_row_alpha << std::endl
+              << "fix col z: " << fix_col_z << std::endl
+              << "fix row z: " << fix_row_z << std::endl
+              << "fix hyperparamters: " << fix_hypers << std::endl
+              << "seed: " << seed << std::endl;
+    std::cout << "=================================================" << std::endl;
+    std::cout << "rows: " << rows << std::endl
+              << "iters: " << iters << std::endl
+              << "chains: " << chains << std::endl
+              << "lag: " << lag << std::endl;
+    std::cout << "=================================================" << std::endl;
+
+    std::vector<std::string> datatypes = {"categorical", "continuous", "categorical", 
+                                          "continuous", "categorical"};
+
+    // std::vector<std::string> datatypes = {"continuous"};
+
+	baxcat::GewekeTester gwk(rows, datatypes.size(), datatypes, seed, !fix_hypers, !fix_row_alpha, 
+                             !fix_col_alpha, !fix_row_z, !fix_col_z);
+
+	gwk.run(iters, chains, lag);
 
 	gwk.outputResults();
 
