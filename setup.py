6a6f111ece869d1cef74ec4cc551553074122a41
Baxter Eaves <baxyjr@gmail.com>
2016-08-27 19:09:16 -0500

    Fix setup.py so it at least doesn't break on normal install

--- setup.py
+++ setup.py
@@ -16,11 +16,15 @@ reqs = [str(ir.req) for ir in install_reqs]
 SRC = os.path.join('cxx', 'src')
 INC = os.path.join('cxx', 'include')
 
-iboost = ''
-if os.environ.get('READTHEDOCS', None) is not None:
-    os.system("wget https://sourceforge.net/projects/boost/files/boost/1.61.0/boost_1_61_0.tar.gz")
+baxcat_compile_args = ['-std=c++11', '-Wno-comment', '-fopenmp']
+
+if os.environ.get('READTHEDOCS', None) == 'True':
+    import urllib
+    urllib.urlretrieve(
+        "https://sourceforge.net/projects/boost/files/boost/1.61.0/boost_1_61_0.tar.gz",
+        "boost_1_61_0.tar.gz")
     os.system("tar -zxf boost_1_61_0.tar.gz")
-    iboost = '-I ' + os.path.join(here, 'boost_1_61_0')
+    baxcat_compile_args.append('-I' + os.path.join(here, 'boost_1_61_0'))
 
 
 extensions = [
@@ -31,7 +35,7 @@ extensions = [
                        os.path.join(SRC, 'categorical.cpp'),
                        os.path.join(SRC, 'continuous.cpp'),
                        os.path.join(SRC, 'feature_tree.cpp')],
-              extra_compile_args=['-std=c++11', '-Wno-comment', '-fopenmp', iboost],
+              extra_compile_args=baxcat_compile_args,
               extra_link_args=['-lstdc++', '-fopenmp'],
               include_dirs=[SRC, INC, np.get_include()],
               language="c++"),
