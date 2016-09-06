88c1036b86c706adf8fa64ca43cad2956f6a5ec5
Baxter Eaves <baxyjr@gmail.com>
2016-09-06 14:11:28 -0500

    Test parameter tweeks

--- baxcat/test/inftest_geweke.py
+++ baxcat/test/inftest_geweke.py
@@ -11,5 +11,5 @@ resdir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'result')
 def test_geweke(seed):
     dtypes = ['categorical']*5 + ['continuous']*5
     gwk = Geweke(10, 10, dtypes, seed, ct_kernel=0, m=1)
-    gwk.run(2000, 5, 1)
+    gwk.run(5000, 5, 1)
     assert not gwk.output(resdir)
